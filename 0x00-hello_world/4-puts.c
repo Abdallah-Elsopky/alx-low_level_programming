b461ca828a3b30e34ec53f466b1871d11aafdd04
your_username <your_email@example.com>
2023-10-06 09:22:51 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
 /**
- * Main - Entry poient
- *
- * Description: A C program that prints with put function
- *
- * Return: Alawys 0 (Success)
-*/
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
 
 int main(void)
 {
-        puts("Programming is like building a multilingual puzzle);
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
