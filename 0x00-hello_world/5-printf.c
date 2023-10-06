91bb5ded0fce283e956ed2e7a1437f63b0c78354
your_username <your_email@example.com>
2023-10-06 09:32:49 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A c program that prints srting "with proper grammar, but the outcome is a piece of art," with printf
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+
+}
