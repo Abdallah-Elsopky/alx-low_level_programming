cd2d805286e089f3698638300210ed192de40f8d
your_username <your_email@example.com>
2023-10-09 11:05:11 -0700

    last task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,10 @@
  * 
  * Description: this print text without printf or puts and return new line
  * 
- * Reurn: 1
- */
+ * Return: 1
+*/
 int main(void) {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-		return (1);
+	char quo [] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1,quo,59);
+	return (1);
 }
