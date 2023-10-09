6706dfd1754c3cd228f2a06a2dbd0df2ff0faa84
your_username <your_email@example.com>
2023-10-09 11:16:18 -0700

    last task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,15 @@
 #include <unistd.h>
+
 /**
- * main - Entry Point
- * 
- * Description: this print text without printf or puts and return new line
- * 
- * Return: 1
-*/
-int main(void) {
-	char quo [] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1,quo,59);
+ *main - Entry Point
+ *
+ *Description: print a qoute using write function witout printf or puts
+ *
+ *Return: 1 (not success)
+ */
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, quo, 59);
 	return (1);
 }
