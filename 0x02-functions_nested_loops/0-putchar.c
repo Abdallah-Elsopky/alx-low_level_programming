c74be69904dd260920b443c01880a686ecbd2a74
your_username <your_email@example.com>
2023-10-10 12:29:48 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
+
+/**
+ * _putchar - writes a character to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ */
+int _putchar(char c)
+{
+    return (write(1, &c, 1));
+}
+
+/**
+ * main - prints "_putchar", followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	printf("_putchar\n");
-		return 0;
+    char s[] = "_putchar\n";
+    int i = 0;
+
+    /* Loop through the string and print each character */
+    while (s[i])
+    {
+        _putchar(s[i]);
+        i++;
+    }
+
+    /* Return 0 to indicate success */
+    return (0);
 }
+
