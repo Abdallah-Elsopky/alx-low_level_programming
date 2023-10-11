f2346e4dffa673da0285c4b2dd6656a8987b2b11
your_username <your_email@example.com>
2023-10-11 15:42:19 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,34 +1,17 @@
 #include <stdio.h>
 
 /**
- * _putchar - writes a character to stdout
- * @c: The character to print
+ * main - Entry point
  *
- * Return: On success 1.
- */
-int _putchar(char c)
-{
-    return (write(1, &c, 1));
-}
+ * Description: print _putchar
+ *
+ * Return: 0 (success)
+*/
 
-/**
- * main - prints "_putchar", followed by a new line
- *
- * Return: Always 0 (Success)
- */
+#include <stdio.h>
+
 int main(void)
 {
-    char s[] = "_putchar\n";
-    int i = 0;
-
-    /* Loop through the string and print each character */
-    while (s[i])
-    {
-        _putchar(s[i]);
-        i++;
-    }
-
-    /* Return 0 to indicate success */
-    return (0);
+	printf("_putchar\n");
+	return (0);
 }
-
