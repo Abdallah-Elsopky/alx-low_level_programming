1d8cfc947d0479ec71bed1f474ce7e94b934da5e
Abdallah-Elsopky <https://github.com/Abdallah-Elsopky>
2024-06-21 03:17:46 +0300

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,14 @@
-#include <stdio.h>
 /**
  * main - Entry point
  *
+ * Description: c program using printf and add new line
+ *
  * Return - Always 0 (Success)
  */
+#include <stdio.h>
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
