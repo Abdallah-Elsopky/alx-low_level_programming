2013f41c73ba87c6f12fa6a8dc505d29f62a410f
Abdallah-Elsopky <https://github.com/Abdallah-Elsopky>
2024-06-23 10:33:45 +0300

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,24 +12,23 @@
 int main(void)
 {
 	int n;
+	int last;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int last;
+
+	last = n % 10;
 
 	if (last > 5)
 	{
-		last = n % 10;
 		printf("last of %d is %d and is greater than 5\n", n, last);
 	}
-	else if (last > 6 && last != 0)
+	else if (last < 6 && last != 0)
 	{
-		last = n % 10;
 		printf("last of %d is %d and is less than 6 and not 0\n", n, last);
 	}
 	else
 	{
-		last = n % 10;
 		printf("last of %d is %d and is 0\n", n, last);
 	}
 	return (0);
