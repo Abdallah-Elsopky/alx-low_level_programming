36f4b530f6a61a526307eb75469f0deaa5f6a12c
Abdallah-Elsopky <https://github.com/Abdallah-Elsopky>
2024-06-26 05:57:14 +0300

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,15 +8,15 @@
 void print_alphabet_x10(void)
 {
 	int line;
-	char c;
+	int c;
 
 	for (line = 0; c <= 9; line++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar (c);
-			_putchar ('\n');
-		}
 
+		}
+	_putchar ('\n');
 	}
 }
