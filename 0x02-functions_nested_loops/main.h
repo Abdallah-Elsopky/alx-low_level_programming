355e347a2effeb96f7ae3dbd205f9d0de04b6b9e
Abdallah-Elsopky <https://github.com/Abdallah-Elsopky>
2024-06-26 03:21:00 +0300

    play what I call the alphabet game

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,6 +2,17 @@
 #define MAIN_H
 
 int _putchar(char c);
-void print_alphabet(void);
+
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
 
 #endif /* MAIN_H */
