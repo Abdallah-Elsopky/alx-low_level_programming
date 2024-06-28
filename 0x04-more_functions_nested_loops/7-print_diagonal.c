fdbe4876731b60a44d0e4f8533a7b45a0ba05c1e
Abdallah-Elsopky <https://github.com/Abdallah-Elsopky>
2024-06-28 20:48:21 +0300

    amazing draw

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line on the terminal
+ * @n: numbers of diagonals
+ * Return: 0 successful
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar ('\n');
+	}
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar (' ');
+		}
+		_putchar('\\');
+
+	}
+	_putchar ('\n');
+}
