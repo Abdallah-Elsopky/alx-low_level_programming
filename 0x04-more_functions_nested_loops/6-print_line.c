bb966e2bb2172e8a6f5b6d7f0958759b3340a0fc
Abdallah-Elsopky <https://github.com/Abdallah-Elsopky>
2024-06-28 19:06:59 +0300

    s

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,20 +1,22 @@
 #include "main.h"
 
 /**
- * 
+ * print_line - Prototype for draw _
+ * @n: Print numbers of _ that should show off
+ * Return: 0 successful
  */
 
 void print_line(int n)
 {
-    int i;
+	int i;
 
-    if (n <= 0)
-    {
-        _putchar ('\n');
-    }
-    for (i = 1; i <= n; i++)
-    {
-        _putchar ('_');
-    }
-    _putchar ('\n');
+	if (n <= 0)
+	{
+		_putchar ('\n');
+	}
+	for (i = 1; i <= n; i++)
+	{
+		_putchar ('_');
+	}
+	_putchar ('\n');
 }
