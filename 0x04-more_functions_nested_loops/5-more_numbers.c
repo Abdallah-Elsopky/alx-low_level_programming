b0ba77f8d5265faa744fd95a22b0adf01760affe
Abdallah-Elsopky <https://github.com/Abdallah-Elsopky>
2024-06-28 18:32:55 +0300

    Numbers constitute the only universal language

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,16 +6,15 @@
  */
 
 void more_numbers(void)
-{
 int i, j;
 
-	for (i = '0'; i <= '10'; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = '0', j <= '14', j++)
+		for (j = 0; j < 15; j++)
 		{
-			_putchar (j);
+			if (b > 9)
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
 		}
-		_putchar (i);
+		_putchar('\n');
 	}
-	_putchar ('\n');
-}
