0cca51e5a6e2aff31acb50e2ab71b34ed79b993e
Abdallah-Elsopky <https://github.com/Abdallah-Elsopky>
2024-06-28 20:57:13 +0300

    amazing draw

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,15 +8,21 @@
 
 void print_line(int n)
 {
-	int i;
+	int i, j;
 
 	if (n <= 0)
 	{
 		_putchar ('\n');
 	}
-	for (i = 0; i <= n; i++)
+	else
 	{
-		_putchar ('_');
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= i; j++)
+			{
+				_putchar ('_');
+			}
+			_putchar ('\n');
+		}
 	}
-	_putchar ('\n');
 }
