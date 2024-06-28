382f67853046c5ef5a02e709a5f3eaa65fc30e39
Abdallah-Elsopky <https://github.com/Abdallah-Elsopky>
2024-06-28 21:17:10 +0300

    square pants

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - a square fuction
+ * @size: Length and width
+ * Return: 0 successful
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar ('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar ('#');
+			}
+			_putchar ('\n');
+		}
+		_putchar ('\n');
+	}
+}
