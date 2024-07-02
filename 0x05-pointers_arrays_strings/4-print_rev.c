8559817fd722ea4a0f237031b7385ceb5437dacf
Abdallah-Elsopky <https://github.com/Abdallah-Elsopky>
2024-07-02 22:35:52 +0300

    reversing strings prototybe

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - reversing a string
+ * @s: a pointer of array that print the string from user
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
