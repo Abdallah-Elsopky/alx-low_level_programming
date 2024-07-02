95c2dd19cd99b915fcc7225130a824bdb831eacd
Abdallah-Elsopky <https://github.com/Abdallah-Elsopky>
2024-07-03 02:48:00 +0300

    every other character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,22 +2,23 @@
 #include <string.h>
 
 /**
- * 
+ * puts2 - print every 2 chars
+ * @str: result
  */
 
 void puts2(char *str)
 {
-    int i;
-    int len = 0;
+	int i;
+	int len = 0;
 
-    while (str[len] != '\0')
-    {
-        len++;
-    }
+	while (str[len] != '\0')
+	{
+		len++;
+	}
 
-    for (i = 0; i < len; i += 2)
-    {
-        _putchar(str[i]);
-    }
-    _putchar('\n');
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
