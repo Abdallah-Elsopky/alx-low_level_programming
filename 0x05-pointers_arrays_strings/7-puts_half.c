4985633458449ba44e2d3978818777a7a1dcdd7d
Abdallah-Elsopky <https://github.com/Abdallah-Elsopky>
2024-07-03 02:57:32 +0300

    last half of string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,10 +1,33 @@
 #include "main.h"
 
 /**
- * 
+ * puts_half - prints last half of a string
+ * @str: long of our string or half in out sitution
  */
 
 void puts_half(char *str)
 {
+	int i, start;
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		start = len / 2;
+	}
+	else
+	{
+		start = (len - 1) / 2 + 1;
+	}
+
+	for (i = start; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 
 }
