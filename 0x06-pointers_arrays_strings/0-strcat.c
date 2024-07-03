0499d16a1198c0ed16d154d2bdd8bc2efaa8e0d9
Abdallah-Elsopky <https://github.com/Abdallah-Elsopky>
2024-07-03 21:42:38 +0300

    strcat func

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,6 +10,7 @@
 
 char *_strcat(char *dest, char *src)
 {
+	int i;
 	int len = 0;
 	int len2 = 0;
 
@@ -27,5 +28,5 @@ char *_strcat(char *dest, char *src)
 
 	dest[len] = '\0';
 
-	return (len);
+	return (dest);
 }
