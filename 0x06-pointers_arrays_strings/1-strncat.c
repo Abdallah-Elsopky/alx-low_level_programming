e84442d985c03afcac230f562a2a9a4d6efc03ad
Abdallah-Elsopky <https://github.com/Abdallah-Elsopky>
2024-07-04 19:32:32 +0300

    strncat function clone

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat - cpy string 2 to 1 with [n]Bytes
+ * @dest: first string and the pointer
+ * @src: second string that will be added
+ * @n: nums of Bytes that will be added
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len = 0;
+	int i;
+
+	while (dest[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[len + i] = src[i];
+	}
+		dest[len = '\0'];
+
+	return (dest);
+}
