807a78b1e82c38d540dfb2d2a63cefd2501e09e1
Abdallah-Elsopky <https://github.com/Abdallah-Elsopky>
2024-07-12 00:08:17 +0300

    rev_string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print string reversed
+ * @s: len of string
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		_print_rev_recursion(*s + 1);
+		_putchar(*s);
+	}
+}
