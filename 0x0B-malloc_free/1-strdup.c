1a271b50036770dc339e1c63a0b8d8981dd17bb2
Abdallah-Elsopky <https://github.com/Abdallah-Elsopky>
2024-07-17 01:22:28 +0300

    The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,31 +9,24 @@
  */
 
 char *_strdup(char *str)
-	{
+{
+	char *strnew = NULL;
+	unsigned int i;
+	int n;
+
 	if (str == NULL)
+		return (NULL);
+	for (n = 0; str[n] != '\0'; n++)
+		;
+	strnew = (char *)malloc(n + 1 * sizeof(char));
+	if (strnew != NULL)
+	{
+		for (i = 0; str[i] != '\0'; i++)
+			strnew[i] = str[i];
+	} else
 	{
 		return (NULL);
 	}
-
-	int length = 0;
-
-	while (str[length] != '\0')
-	{
-		length++;
-	}
-
-	char *copy = malloc((length + 1) * sizeof(char));
-
-	if (copy == NULL)
-	{
-		return (NULL);
-	}
-
-	for (int i = 0; i < length; i++)
-	{
-		copy[i] = str[i];
-	}
-	copy[length] = '\0';
-
-	return (copy);
+	strnew[i] = '\0';
+	return (strnew);
 }
