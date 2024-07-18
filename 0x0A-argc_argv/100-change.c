e0290e966898b35ab17b78ed81ae44481c88f726
Abdallah-Elsopky <https://github.com/Abdallah-Elsopky>
2024-07-18 06:51:08 +0300

    Coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,42 +11,29 @@
 
 int main(int argc, char *argv[])
 {
-	int change = 0;
-	int n;
+	int change = 0, n;
 
 	if (argc != 2)
 	{
-		printf("Error\n");
-		return (1);
+	printf("Error\n");
+	return (1);
 	}
-
 	n = atoi(argv[1]);
-
 	if (n < 0)
-	{
-		printf("0\n");
-		return (0);
-	}
+	{ printf("0\n");
+	return (0); }
 	if (n >= 25)
-	{
-		change = n / 25;
-		n = n % 25;
-	}
+	{ change = n / 25;
+	n = n % 25; }
 	if (n >= 10)
-	{
-		change += n / 10;
-		n = n % 10;
-	}
+	{ change += n / 10;
+	n = n % 10; }
 	if (n >= 5)
-	{
-		change += n / 5;
-		n = n % 5;
-	}
+	{ change += n / 5;
+	n = n % 5; }
 	if (n >= 2)
-	{
-		change += n / 2;
-		n = n % 2;
-	}
+	{ change += n / 2;
+	n = n % 2; }
 	change += n;
 	printf("%d\n", change);
 return (0);
