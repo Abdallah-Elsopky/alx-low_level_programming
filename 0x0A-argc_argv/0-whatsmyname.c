86c3b46450c24565e2a444f9ed39ba295ece1892
Abdallah-Elsopky <https://github.com/Abdallah-Elsopky>
2024-07-18 04:12:50 +0300

    prints my name as a file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entary Point
+ * @argc: number of command line arguments
+ * @argv: array containing the program command line arguments
+ *
+ * Return: (0) successful.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
