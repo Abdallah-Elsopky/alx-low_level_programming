d38ba7cae06652f67d32a1a3fd07a1e316b56928
Abdallah-Elsopky <https://github.com/Abdallah-Elsopky>
2024-07-18 04:35:32 +0300

    print nums of arguments you entering

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 /**
- * 
+ * main - Entary Point
+ * @argc: number of command line arguments
+ * @argv: array containing the program command line arguments
+ *
+ * Return: (0) successful.
  */
 
 int main(int argc __attribute__((unused)), char argv[])
