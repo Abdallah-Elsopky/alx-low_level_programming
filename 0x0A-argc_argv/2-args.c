8d042ec3797bc59ef18853ab5bfa97c2b98c50d6
Abdallah-Elsopky <https://github.com/Abdallah-Elsopky>
2024-07-18 04:55:33 +0300

    receives arguments you entaring

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entary Point
+ * @argc: number of command line arguments
+ * @argv: array containing the program command line arguments
+ *
+ * Return: (0) successful.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+}
