85ba348ac9ec3f6e5be634445364c83d3428b16c
Abdallah-Elsopky <https://github.com/Abdallah-Elsopky>
2024-07-24 05:36:11 +0300

    header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - Data
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: struct of data about dog
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+
+#endif /* MAIN_H */
