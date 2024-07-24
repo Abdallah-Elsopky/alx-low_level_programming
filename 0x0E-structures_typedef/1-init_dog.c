eb3a0953e842b06ea1713d5f027a29a979816dc2
Abdallah-Elsopky <https://github.com/Abdallah-Elsopky>
2024-07-24 05:41:48 +0300

    struct dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * init_dog - structure
+ * @d: struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	{
+		return (1);
+	}
+	else
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,5 +17,7 @@ typedef struct dog
 	char *owner;
 } dog_t;
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 
 #endif /* MAIN_H */
