17bfcce476525a24b444aac72e39f03b1f9e4264
Abdallah-Elsopky <https://github.com/Abdallah-Elsopky>
2024-07-24 06:05:43 +0300

    new dog status

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,38 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - Entary point
+ * @name: new dog's name
+ * @age: new dog's age
+ * @owner: new dog's owner
+ * Return: p
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *p;
+
+	p = malloc(sizeof(dog_t));
+		if (p == NULL)
+		{
+			return (NULL);
+		}
+		if (name == NULL)
+		{
+			free(p);
+			free(owner);
+			return (NULL);
+		}
+		if (owner == NULL)
+		{
+			free(p);
+			free(name);
+			return (NULL);
+		}
+	p->name = name;
+	p->age = age;
+	p->owner = owner;
+
+	return (p);
+}
