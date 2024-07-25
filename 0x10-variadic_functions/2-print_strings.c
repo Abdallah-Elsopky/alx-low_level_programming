3d75e4143dc1a768010eee3f24ad24d41333e412
Abdallah-Elsopky <https://github.com/Abdallah-Elsopky>
2024-07-25 15:42:53 +0300

    string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-#include <stdarg.h>
-#include <stdlib.h>
+#include "variadic_functions.h"
 
 /**
  * print_strings - string
@@ -10,21 +9,27 @@
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	int i;
-	char *s;
-	va_list args;
+	unsigned int i;
+	char *str;
 
-	va_start(args, n);
+	va_list list;
+
+	va_start(list, n);
+
 	for (i = 0; i < n; i++)
 	{
-		s = va_arg(args, char *);
-		if (s != NULL)
-			printf("%s", s);
+		str = va_arg(list, char *);
+		if (!str)
+			str = "(nil)";
+		if (!separator)
+			printf("%s", str);
+		else if (separator && i == 0)
+			printf("%s", str);
 		else
-			printf("(nil)");
-		if (separator != NULL && i < n - 1)
-			printf("%s", separator);
+			printf("%s%s", separator, str);
 	}
+
 	printf("\n");
-	va_end(args);
+
+	va_end(list);
 }
