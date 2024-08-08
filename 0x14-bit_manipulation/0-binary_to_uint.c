b60107cdf1256ab6f3f3ec4741a002b3599db14e
Abdallah-Elsopky <https://github.com/Abdallah-Elsopky>
2024-08-08 23:54:11 +0300

    this is a unit calculator

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,20 +9,18 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
+	unsigned int decimal;
 	unsigned int i;
 
-	for (i = 0; b[i] != '\0'; i++)
+	for (decimal = 0, i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] == '1')
-			result = (result << 1) | 1;
+			decimal = (decimal << 1) | 1;
 		else if (b[i] == '0')
-			result <<= 1;
+			decimal <<= 1;
 		else if (b[i] != '0' && b[i] != '1')
 			return (0);
 	}
 
-
-	return (result);
-
+	return (decimal);
 }
