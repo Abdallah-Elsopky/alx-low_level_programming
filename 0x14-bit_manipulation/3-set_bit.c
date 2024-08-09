a42b38cd826072c73db35fc5df4c4aeb6340d0b1
Abdallah-Elsopky <https://github.com/Abdallah-Elsopky>
2024-08-09 04:00:04 +0300

    set bitz

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * set_bit - swap num you want to 1
+ * @n: num of index
+ * @index: pointeing to the bit we want to change
+ *
+ * Return: 1 success or -1 if fail
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+
+		return (!!(*n |= 1l << index));
+}
