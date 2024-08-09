767be8c6b6a7432bdb7f73015adc98a5c44beffa
Abdallah-Elsopky <https://github.com/Abdallah-Elsopky>
2024-08-09 04:14:16 +0300

    clear bitz

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * set_bit - swap num you want to 0
+ * clear_bit - swap num you want to 0
  * @n: num of index
  * @index: pointeing to the bit we want to change
  *
@@ -10,12 +10,12 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-    unsigned long int i = ~(1 << index);
+	unsigned long int i = ~(1 << index);
 
 	if (index >= sizeof(n) * 8)
 		return (-1);
 
-    *n = *n & i;
+	*n = *n & i;
 
-    return (1);
+	return (1);
 }
