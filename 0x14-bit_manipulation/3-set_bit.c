ad23b2a9d395640f49db02c47103a7cd32c2e7be
Abdallah-Elsopky <https://github.com/Abdallah-Elsopky>
2024-08-09 04:02:58 +0300

    set bitz

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,8 +10,10 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int i;
+
 	if (index >= sizeof(n) * 8)
 		return (-1);
-
-		return (ii(*n |= 1l << index));
+	*n = *n | i;
+	return (1);
 }
